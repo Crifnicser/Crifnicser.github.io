35393b3a3133997b2aab58c01dd0a07b113205c0
Crifnicser <56223758+Crifnicser@users.noreply.github.com>
2020-02-16 17:20:28 +0200

    Add files via upload

---
+++ js/js/script.js
@@ -0,0 +1,81 @@
+// let str="Привіт";
+// let num=123;
+// let flag=true;
+// let txt="true";
+// console.log (typeof(str));
+// console.log (typeof(num));
+// console.log (typeof(flag));
+// console.log (typeof(txt));
+
+// let str="Привіт";
+// let num=122;
+// let flag=true;
+// let array=[];
+// let object={};
+// const txt="hello";
+// const nuh=321;
+// const ryt=false;
+// const b=[];
+// const a={};
+// console.log (typeof(str));
+// console.log (typeof(num));
+// console.log (typeof(flag));
+// console.log (typeof(array));
+// console.log (typeof(object));
+// console.log (typeof(txt));
+// console.log (typeof(nuh));
+// console.log (typeof(ryt));
+// console.log (typeof(b));
+// console.log (typeof(a));
+
+//  let myAge=13;
+//  let age=14;
+//  let a=myAge+age;
+//  alert (a);
+
+// let a=108
+//     b=223
+//     n=2
+//     m=5
+//     d=a+b-2*5
+// console.log (d);
+
+// let a1=5+3
+//     a2=5-3
+//     a3=5*3
+//     a4=5/3
+//     a5=a1+a2+a3+a4;
+// console.log (a1);
+// console.log (a2);
+// console.log (a3);
+// console.log (a4);
+// console.log (a5);
+
+// let a6=5%3
+//     a7=3%5
+//     a8=5+'3'
+//     a9='5'-3
+//     a10=75+'кг'
+//     alert (a6);
+//     alert (a7);
+//     alert (a8);
+//     alert (a9);
+//     alert (a10);
+
+// let heigth=23;
+//     weith=10;
+//     s=heigth*weith
+// console.log (s);  
+
+// let heigthC=10;
+//     dC=4;
+//     radius= dC/2;
+//     p=3.14;
+//     v=p*Math.pow(radius,2)*heigthC;
+//     alert (v);
+
+    
+// let n=3;
+//     m=4;
+//     k=Math.pow(n,2)+Math.pow(m,2)
+//     alert (k);
