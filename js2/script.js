2576b0d8bd58dd7e440d3b6ddff9eb8d219198a8
Crifnicser <56223758+Crifnicser@users.noreply.github.com>
2020-02-23 17:49:49 +0200

    Add files via upload

---
+++ js2/script.js
@@ -0,0 +1,33 @@
+//задача 1
+// let k1= '5px';
+//     k2= '12djd334';
+//     k3='12.45asdwe12';
+//     k4='qwqweeewq';
+//     console.log (Number.parseInt(k1));
+//     console.log (Number.parseInt(k2));
+//     console.log (Number.parseInt(k3));
+//     console.log (Number.parseInt(k4));
+
+
+//2
+// let k1= '5px';
+//     k2= '12djd334';
+//     k3='12.45asdwe12';
+//     k4='qwqweeewq';
+//     console.log (Number.parseFloat(k1));
+//     console.log (Number.parseFloat(k2));
+//     console.log (Number.parseFloat(k3));
+//     console.log (Number.parseFloat(k4));
+
+
+//3
+// let random=Math.floor(Math.random()*(10-1)+1);
+// console.log(Math.pow(random,2));
+
+
+//4
+// const a='My ';
+//       b='name';
+//       c=' is ';
+//       fullName=`${a}${b}${c}Viktor!`;
+//       console.log(fullName);
