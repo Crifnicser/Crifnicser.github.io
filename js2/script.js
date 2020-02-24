2f5b63109b4bc022eb004de44925020d18769e23
Crifnicser <56223758+Crifnicser@users.noreply.github.com>
2020-02-24 12:02:41 +0200

    Add files via upload

--- js2/script.js
+++ js2/script.js
@@ -31,3 +31,34 @@
 //       c=' is ';
 //       fullName=`${a}${b}${c}Viktor!`;
 //       console.log(fullName);
+
+//5
+// const result=5+5+'5';
+// console.log(result);
+
+
+// 6
+// const num=20;
+//       result = num > 10 && num < 30;
+//       console.log (result);
+
+
+// 7
+// const num=40;
+//     result= num < 10 || num > 30; 
+//     console.log (result);
+
+// 8
+// console.log (!true);
+// console.log (!false);
+// console.log (!1);
+// console.log (!0);
+
+
+// 9
+let a=5;
+    b=10;
+    c=5;
+    x=0;
+    result=Math.pow (ax,2)+bx+c;
+console.log (result);
