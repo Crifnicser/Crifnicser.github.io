e684de821aed947bbc83dfac8164dc538dad7b20
Crifnicser <56223758+Crifnicser@users.noreply.github.com>
2020-03-04 10:13:25 +0200

    Add files via upload

---
+++ js3/js/script.js
@@ -0,0 +1,81 @@
+// 1
+// const a=1;
+// if (a>2){
+//     console.log("сьогодні перше число");
+// }else{
+//     console.log("сьогодні ",a);
+// }
+
+
+// 2
+// const a=1;
+// if (a%2==0){
+// console.log (a);
+// }else {
+//     console.log("upppsss");
+// }
+
+
+//3
+// let c=5%2;
+// if (c==0){
+// console.log(c+" грн");
+// }else {
+//     console.log(c+" euro")
+// }
+
+
+//4
+// let Macks=55;
+// if (Macks>0 && Macks<17){
+//     console.log('kids');
+// }else if(Macks>16 && Macks<61){
+// console.log ('adult');
+// }else {
+//     console.log ('old');
+// }
+
+
+//5
+// let a=Math.floor(Math.random() * 2),
+//     b=Math.floor(Math.random() * 2);
+// if (a==b) {
+// console.log("ФАРТ");
+// } else {
+//     console.log("НЕ ФАРТ");
+// }
+
+
+//6
+// let name="Микита",
+//     surname="Янкевич";
+// if (name.length>4 && surname.length>5){
+// console.log(name.length+surname.length);
+// }else{
+//     console.log("УПС");
+// }
+
+
+//7
+// let a=Math.floor(Math.random() * 5)+1;
+// if (a==1){
+//     console.log ("це число 1");
+// }else if (a==2){
+//     console.log ("це число 2");
+// }else if (a==3){
+//     console.log ("це число 3");
+// }else if (a==4){
+//     console.log ("це число 4");
+// }else if (a==5){
+//     console.log ("це число 5");
+// }
+
+
+//8
+// let yourAge=prompt("Моєму сусіду зліва: "),
+//     myAge=13;
+// if (yourAge>12){
+//     console.log (myAge+Number.parseInt(yourAge));
+// }else {
+//     console.log ("Мужик ти попав не туда");
+// }
