380963b970c5350791275693685881721dc6eaf5
Crifnicser <56223758+Crifnicser@users.noreply.github.com>
2020-03-08 17:57:02 +0200

    Add files via upload

---
+++ js4/js/script.js
@@ -0,0 +1,64 @@
+//1
+// let a = 0;
+// while (a< 11) {
+//   console.log(a);
+//    a +=1;
+// }
+
+//2
+// for (let i=0; i<11; i++){
+//     if (i%2==0){
+//         console.log('число парне');
+//     }else{
+//         console.log('число не парне');
+//     }
+// }
+
+
+//3
+// let num=20;
+// while (num<29) {
+//   console.log(num);
+//   num+=1;
+// }
+
+
+//4
+// let name='';
+// do {
+//   name= prompt('Введіть вашого ульбленого героя', '');
+// } while (name.length > 6);
+// console.log(name);
+
+
+//5
+// let arr =[5,3,6,2,4],
+// result=0;
+// for (let i=0; i<arr.length; i++){
+//     console.log(arr[i]);
+//     result+=arr[i];
+// }
+// console.log("Сума: "+result);
+
+
+//6
+// let a=[-1,3,16,-2,-3,5],
+// magic=0;
+// if (a<0){
+//     console.log
+// }
+// for (let i=0; i<a.length; i++){
+//     if (a[i]>0){
+//     magic+=a[i];
+//     }
+// }
+// console.log("Сума: "+magic);
+
+
+//7
+let big=[1,2,3,4,5,6];
+str=''
+for (let i=0; i<big.length; i++){
+    str +=  big[i]+"-";
+}
+console.log(str);
