3e8c6339ceb2b0b4eca34ad904f0a9f9e32dbc3a
Crifnicser <56223758+Crifnicser@users.noreply.github.com>
2020-03-15 18:17:16 +0200

    Add files via upload

---
+++ js5/js/script.js
@@ -0,0 +1,68 @@
+//1
+// let Arr1=[1, 5, "4", "hello"],
+//     Arr2=[true,2, {}, ["a"], 222];
+// console.log(Arr1[1]);
+// console.log(Arr2[1]);
+// console.log(Arr1[1]+Arr2[1]);
+
+
+//2
+let Arr1=[1, 5, "4", "hello"],
+    Arr2=[true,2, {}, ["a"], 222];
+Arr1[4]=22;
+console.log(Arr1);
+
+
+//3
+// let Arr2=[true,2, {}, ["a"], 222];
+// for(let Arr of Arr2){
+// console.log(Arr);
+// console.log(typeof Arr);
+// }
+
+
+//4
+// const matrix = [[10,11,12],
+//                 [13,14,15,],
+//                 [16,17,18] 
+//             ];
+// for (let i=0; i<matrix.length; i++){
+//     console.log (matrix[i]);
+//     for (let m=0; m<matrix[i].length; m++){
+//         console.log(matrix[i][m]);
+//     }
+// }
+
+
+//5
+// const message = 'Welcome to Ukraine!';
+// let mr=message.split('');
+// console.log(mr);
+// console.log(mr.indexOf('l'));
+
+
+//6
+// let stack=[],
+//  stack=[2,3,4],
+//  stk=[5,6,7,8];
+//  for(let i=0; i<0; i++){
+//      console.log(stack);
+//  }
+
+
+//7
+
+
+
+//8
+// let animals=["pig","dog"],
+//     pets=["cat","parrot", "chicken"]
+//     console.log(pets);
+//     console.log(animals);
+
+
+//9
+// let pets=["cat","parrot",];
+//     pets[2]="shark","elefants";
+//     pets[3]="elefants";
+//     console.log(pets);
