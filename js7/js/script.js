0c9a9ace8c544d8d83e20389cdd03a519f324cf8
Crifnicser <56223758+Crifnicser@users.noreply.github.com>
2020-04-01 14:10:59 +0300

    Add files via upload

---
+++ js7/js/script.js
@@ -0,0 +1,70 @@
+//1
+// const hotel = {
+// name:'NICSER',
+// star:5,
+// marred:false,
+// };
+// console.log(hotel);
+
+
+//2
+// let obj={
+// name:'NICSER',
+// age:13,
+// trex:"FART",
+// marred:false,
+// };
+// delete obj.trex;
+// obj.language='JavaScript';
+// console.log(obj);
+
+
+//3
+// let obj={
+//     first: 1,
+//     second:2,
+// };
+// obj.sum = function(){
+//     console.log(obj.first+obj.second);
+// };
+// obj.sum();
+
+
+//4
+// const hotel = {
+// name:'NICSER',
+// star:5,
+// trex:"FART",
+// mared:false,
+// };
+// let keys= Object.keys(hotel);
+// for (let i=0; i<keys.length; i++){
+//     console.log('value',keys[i]);
+// }
+
+
+//5
+const hotel = {
+name:'NICSER',
+star:5,
+trex:"FART",
+mared:false,
+};
+let keys=Object.keys (hotel);
+for (let i=0; i<keys.length; i++){
+    console.log('value',keys[i]);
+}
+
+
+//6
+const cafe={
+    name:'NICSLER',
+    star:5,
+    klar:100,
+    personal:10,
+    mared:false,
+};
+let keys=Object.keys(cafe);
+for (let i=0; i<keys.length; i++){
+    console.log('value',keys[i]);
+}
